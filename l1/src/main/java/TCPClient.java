0441f8b76cf84af0302f3d8a1f5879943f84ea13
zzhou612 <zzhou612@gmail.com>
2018-05-19 22:19:52 +0800

    feat: support file transfer between client and server

--- l1/src/main/java/TCPClient.java
+++ l1/src/main/java/TCPClient.java
@@ -2,17 +2,26 @@ import java.io.*;
 import java.net.*;
 
 class TCPClient {
-    public static void main(String argv[]) throws Exception {
-        String sentence;
-        String modifiedSentence;
-        BufferedReader inFromUser = new BufferedReader(new InputStreamReader(System.in));
-        Socket clientSocket = new Socket("localhost", 6789);
-        DataOutputStream outToServer = new DataOutputStream(clientSocket.getOutputStream());
-        BufferedReader inFromServer = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
-        sentence = inFromUser.readLine();
-        outToServer.writeBytes(sentence + '\n');
-        modifiedSentence = inFromServer.readLine();
-        System.out.println("FROM SERVER: " + modifiedSentence);
-        clientSocket.close();
+    public static void main(String[] args) throws IOException {
+        Socket socket = new Socket("127.0.0.1", 7650);
+        BufferedReader input = new BufferedReader(
+                new InputStreamReader(socket.getInputStream(), "UTF-8"));
+        PrintWriter output = new PrintWriter(
+                new OutputStreamWriter(socket.getOutputStream(), "UTF-8"), true);
+        output.println("BEGIN;");
+        output.println("QUERY;");
+        File file = new File("/home/zzhou612/Documents/IdeaProjects/socket-playground/data/client-test.txt");
+        FileInputStream fis = new FileInputStream(file);
+        DataOutputStream dos = new DataOutputStream(socket.getOutputStream());
+        output.format("SIZE txt %d;\n", file.length());
+        byte[] bytes = new byte[1024];
+        int length;
+        while ((length = fis.read(bytes, 0, bytes.length)) != -1) {
+            dos.write(bytes, 0, length);
+        }
+        output.println("QUERY;");
+        output.println("END;");
+        socket.close();
+        System.exit(0);
     }
 }
--- l1/src/main/java/TCPServer.java
+++ l1/src/main/java/TCPServer.java
@@ -1,21 +1,88 @@
 import java.io.*;
-import java.net.*;
+import java.net.ServerSocket;
+import java.net.Socket;
 
-class TCPServer {
-    public static void main(String argv[]) throws Exception {
-        String clientSentence;
-        String capitalizedSentence;
-        ServerSocket welcomeSocket = new ServerSocket(6789);
 
-        while (true) {
-            Socket connectionSocket = welcomeSocket.accept();
-            BufferedReader inFromClient =
-                    new BufferedReader(new InputStreamReader(connectionSocket.getInputStream()));
-            DataOutputStream outToClient = new DataOutputStream(connectionSocket.getOutputStream());
-            clientSentence = inFromClient.readLine();
-            System.out.println("Received: " + clientSentence);
-            capitalizedSentence = clientSentence.toUpperCase() + '\n';
-            outToClient.writeBytes(capitalizedSentence);
+public class TCPServer {
+
+    public static void main(String[] args) throws Exception {
+        int clientNumber = 0;
+        try (ServerSocket listener = new ServerSocket(7650)) {
+            while (true) {
+                new Handler(listener.accept(), clientNumber++).start();
+            }
         }
     }
+
+    public static class Handler extends Thread {
+        private Socket socket;
+        private int clientNumber;
+        private String fileName = "server-test";
+        private String fileDir = "/home/zzhou612/Documents/IdeaProjects/socket-playground/data/";
+
+        public Handler(Socket socket, int clientNumber) {
+            this.socket = socket;
+            this.clientNumber = clientNumber;
+        }
+
+        public void run() {
+            try (BufferedReader input = new BufferedReader(
+                    new InputStreamReader(socket.getInputStream(), "UTF-8"));
+                 PrintWriter output = new PrintWriter(
+                         new OutputStreamWriter(socket.getOutputStream(), "UTF-8"), true)) {
+                while (true) {
+                    String command = input.readLine();
+                    output.println("OK");
+                    if (command.startsWith("BEGIN")) {
+                        log("Link starts.");
+                    } else if (command.startsWith("SIZE")) {
+                        String args[] = command.split("\\s|;");
+                        String type = args[1];
+                        int size = Integer.parseInt(args[2]);
+                        saveFile(size, type);
+                    } else if (command.startsWith("QUERY")) {
+                        log("Query...");
+                    } else if (command.startsWith("END")) {
+                        log("Link ends.");
+                        return;
+                    }
+                }
+            } catch (IOException e) {
+                log("Error handling client# " + clientNumber + ": " + e);
+            } finally {
+                try {
+                    socket.close();
+                } catch (IOException e) {
+                    log("Couldn't close a socket.");
+                }
+                log("Connection with client# " + clientNumber + " closed");
+            }
+        }
+
+        private void saveFile(int size, String type) {
+            log("File transfer starts.");
+            log("Size: " + Integer.toString(size));
+            log("Type: " + type);
+            try {
+                DataInputStream dis = new DataInputStream(socket.getInputStream());
+                FileOutputStream fos = new FileOutputStream(fileDir + fileName + "." + type);
+                byte[] buffer = new byte[size];
+                int length;
+                int remaining = size;
+                while ((length = dis.read(buffer, 0, Math.min(buffer.length, remaining))) > 0) {
+                    fos.write(buffer, 0, length);
+                    remaining -= length;
+                }
+                fos.close();
+                log("File transfer end.");
+            } catch (IOException e) {
+                log("Error handling client# " + clientNumber + " in saveFile(): " + e);
+            }
+        }
+
+        private void log(String message) {
+            System.out.println(message);
+        }
+    }
+
 }
