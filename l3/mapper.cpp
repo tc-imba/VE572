4ef060980e9a15197b92f7dc8d454aeb3ddd7640
tc-imba <liuyh615@sjtu.edu.cn>
2018-07-16 23:11:47 +0800

    l3update

--- l3/mapper.cpp
+++ l3/mapper.cpp
@@ -8,14 +8,16 @@
 using namespace std;
 
 int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+
     string str;
     getline(cin, str);
 
-    while (getline(cin, str)) {
-        if (str.empty()) continue;
+    while (getline(cin, str) && !str.empty()) {
         auto p1 = str.find(',');
         auto p2 = str.find(',', p1 + 1);
-        cout << str.substr(p1 + 1, p2 - p1 - 1) << '\t' << str.substr(p2 + 1) << endl;
+        cout << str.substr(p1 + 1, p2 - p1 - 1) << '\t' << str.substr(p2 + 1) << '\n';
     }
 
     return 0;
