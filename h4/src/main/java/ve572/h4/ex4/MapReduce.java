39b71b5707f43294d0b83659617949aa6a4cfe69
tc-imba <liuyh615@sjtu.edu.cn>
2018-08-04 04:16:37 +0800

    p1

---
+++ h4/src/main/java/ve572/h4/ex4/MapReduce.java
@@ -0,0 +1,74 @@
+package ve572.h4.ex2;
+
+import java.io.IOException;
+import java.util.StringTokenizer;
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.FileSystem;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.Mapper;
+import org.apache.hadoop.mapreduce.Reducer;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+
+import static java.lang.Integer.parseInt;
+
+public class MapReduce {
+
+    public static class Map extends Mapper<Object, Text, Text, IntWritable> {
+        private Text id = new Text();
+        private IntWritable score = new IntWritable();
+
+        public void map(Object key, Text value, Context context) throws IOException, InterruptedException {
+            StringTokenizer tokenizer = new StringTokenizer(value.toString(), ",");
+            tokenizer.nextToken();
+            id.set(tokenizer.nextToken());
+            score.set(parseInt(tokenizer.nextToken()));
+
+            context.write(id, score);
+        }
+    }
+
+    public static class Reduce extends Reducer<Text, IntWritable, Text, IntWritable> {
+        private IntWritable result = new IntWritable();
+
+        public void reduce(Text key, Iterable<IntWritable> values, Context context) throws IOException, InterruptedException {
+            int score = 0;
+            for (IntWritable val : values) {
+                score = Math.max(score, val.get());
+            }
+            result.set(score);
+            context.write(key, result);
+        }
+    }
+
+    public static void main(String[] args) throws Exception {
+        Configuration conf = new Configuration();
+        Job job = Job.getInstance(conf, "word count");
+        job.setJarByClass(MapReduce.class);
+        job.setMapperClass(Map.class);
+        job.setCombinerClass(Reduce.class);
+        job.setReducerClass(Reduce.class);
+        job.setOutputKeyClass(Text.class);
+        job.setOutputValueClass(IntWritable.class);
+
+        FileInputFormat.addInputPath(job, new Path(args[0]));
+        Path outputPath = new Path(args[1]);
+        FileSystem fileSystem = outputPath.getFileSystem(conf);
+        if (fileSystem.exists(outputPath)) {
+            fileSystem.delete(outputPath, true);
+        }
+        FileOutputFormat.setOutputPath(job, outputPath);
+
+        long startTime = System.currentTimeMillis();
+        boolean exitCode = job.waitForCompletion(true);
+        long endTime = System.currentTimeMillis();
+        long totalTime = endTime - startTime;
+        System.out.println(totalTime + " ms");
+
+        System.exit(exitCode ? 0 : 1);
+    }
+}
